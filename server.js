3068623b3193ebd05066f370101cd0ec14573b8c
MahmoudAhmed <mahmood.adel@hotmail.com>
2023-11-07 17:57:08 -0500

    modify server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Initialize db by reading the contents of db.json file
 let db = JSON.parse(fs.readFileSync(path.join(__dirname, "db", "db.json")));
